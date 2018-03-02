78aba60502c9eb5913d18b794328f659d737acbf
bigdenbox <bigdenbox@gmail.com>
2018-03-02 15:35:28 +0200

    Change-Id: I99e4ed6835d2d6880cb8e165688d8303824a929b

---
+++ Week1/src/Coursera/MainWordsLengths.java
@@ -0,0 +1,15 @@
+package Coursera;
+
+// import edu.duke.FileResource;
+
+public class MainWordsLengths {
+
+	public static void main(String[] args) {
+
+		WordLengths wordLengths = new WordLengths();
+		wordLengths.testCountWordLengths();
+		
+
+	}
+
+}
