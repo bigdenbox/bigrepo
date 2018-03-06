5fee2f419377952d0b7857ae6c8e638cb81b58ea
boxman <bigdenbox@gmail.com>
2018-03-06 02:15:40 +0200

    Is DONE: In the simpleTests method, add a call to breakCaesarCipher on the encrypted String to decrypt it automatically by determining the key, and print the decrypted String.

--- CouerseraWeek1Last/src/MainCeaserCipher.java
+++ CouerseraWeek1Last/src/MainCeaserCipher.java
@@ -5,7 +5,7 @@ public class MainCeaserCipher {
 
 	public static void main(String[] args) {
 		TestCaesarCipher tcc = new TestCaesarCipher();
-	//	tcc.simpleTests();
+		tcc.simpleTests();
 	//	FileResource file = new FileResource("src\\encryptedRomeo.txt");
 	//	tcc.breakCaesarCipher(file.asString());
 	}
