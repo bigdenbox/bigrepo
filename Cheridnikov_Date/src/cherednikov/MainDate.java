c86d1c07bcacfb64c373eb6618ada13005806f35
boxman <bigdenbox@gmail.com>
2018-04-01 14:43:01 +0300

    Date was changed to real

--- Cheridnikov_Date/src/cherednikov/MainDate.java
+++ Cheridnikov_Date/src/cherednikov/MainDate.java
@@ -6,7 +6,7 @@ public class MainDate {
 		Date date = new Date(01, 04, 2018);
 		System.out.println("Our date is " + date.toString());
 		System.out.println("Day in this year = " + date.getDayOfYear());
-		Date date2 = new Date(01, 04, 20188);
+		Date date2 = new Date(01, 04, 2018);
 		System.out.println("Days beetween " + date + " and " + date2 + " is " + date.daysBetween(date2) + " days");
 		
 		
