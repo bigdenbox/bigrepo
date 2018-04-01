727b7ae7981c0cba3ad4410f56057053a962a85d
boxman <bigdenbox@gmail.com>
2018-04-01 14:56:16 +0300

    ChangeDate

--- Cheridnikov_Date/src/cherednikov/MainDate.java
+++ Cheridnikov_Date/src/cherednikov/MainDate.java
@@ -3,10 +3,10 @@ package cherednikov;
 public class MainDate {
 
 	public static void main(String[] args) {
-		Date date = new Date(01, 04, 2018);
+		Date date = new Date(19, 02, 1983);
 		System.out.println("Our date is " + date.toString());
 		System.out.println("Day in this year = " + date.getDayOfYear());
-		Date date2 = new Date(01, 04, 2018);
+		Date date2 = new Date(19, 02, 1983);
 		System.out.println("Days beetween " + date + " and " + date2 + " is " + date.daysBetween(date2) + " days");
 		
 		
